982998cd9a4a42cce83a2f76ecfb630aac926e48
luo <376693576@qq.com>
2018-10-22 11:59:55 +0800

    之前的细节

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import iconSlup from './assets/icon_slup.png';
 import iconLoading from './assets/loading.gif';
 
 import './index.css';
-
+require('es6-object-assign').polyfill();
 class IscrollLuo extends React.Component {
   constructor(props) {
     super(props);
@@ -16,7 +16,8 @@ class IscrollLuo extends React.Component {
       data: null, // 数据缓存,
       loadingDownShow: false, // 是否处于刷新中状态
       loadingUpShow: false, // 是否属于加载中状态
-      iscrollOptions: { // iscroll 所需参数
+      iscrollOptions: {
+        // iscroll 所需参数
         probeType: 3,
         preventDefault: true,
         click: true,
@@ -39,46 +40,46 @@ class IscrollLuo extends React.Component {
     this.myScroll = null; // 保存当前iscroll实例
   }
 
-    onMouseUpListener = () => {
-      const t = this;
-        window.top.removeEventListener('mouseup', t.onMouseUpListener, false);
-        window.top.removeEventListener('touchend', t.onMouseUpListener, false);
-        // 如果滑动距离超过了设定界限并且当前没在下拉中，就触发下拉
-        if(t.myScroll.y >= t.state.options.beyondHeight && !t.state.loadingDownShow) {
-            if(!t.props.noDown && (t.props.onDown || t.props.onPullDownRefresh)) {
-                t.setState({
-                    loadingDownShow: true,
-                    loadingUpShow: false,
-                });
-                if (t.props.onDown){
-                    t.props.onDown();
-                } else if (t.props.onPullDownRefresh) {
-                    t.props.onPullDownRefresh();
-                }
-            }
-        } else if (t.myScroll.y < t.myScroll.maxScrollY - t.state.options.beyondHeight && !t.state.loadingUpShow) {
-            if(!t.props.noUp && (t.props.onUp || t.props.onPullUpLoadMore)) {
-                t.setState({
-                    loadingDownShow: false,
-                    loadingUpShow: true,
-                });
-                if(t.props.onUp) {
-                    t.props.onUp();
-                } else if (t.props.onPullUpLoadMore) {
-                    t.props.onPullUpLoadMore();
-                }
-            }
+  onMouseUpListener = () => {
+    const t = this;
+    window.top.removeEventListener('mouseup', t.onMouseUpListener, false);
+    window.top.removeEventListener('touchend', t.onMouseUpListener, false);
+    // 如果滑动距离超过了设定界限并且当前没在下拉中，就触发下拉
+    if (t.myScroll.y >= t.state.options.beyondHeight && !t.state.loadingDownShow) {
+      if (!t.props.noDown && (t.props.onDown || t.props.onPullDownRefresh)) {
+        t.setState({
+          loadingDownShow: true,
+          loadingUpShow: false,
+        });
+        if (t.props.onDown) {
+          t.props.onDown();
+        } else if (t.props.onPullDownRefresh) {
+          t.props.onPullDownRefresh();
         }
-    };
+      }
+    } else if (t.myScroll.y < t.myScroll.maxScrollY - t.state.options.beyondHeight && !t.state.loadingUpShow) {
+      if (!t.props.noUp && (t.props.onUp || t.props.onPullUpLoadMore)) {
+        t.setState({
+          loadingDownShow: false,
+          loadingUpShow: true,
+        });
+        if (t.props.onUp) {
+          t.props.onUp();
+        } else if (t.props.onPullUpLoadMore) {
+          t.props.onPullUpLoadMore();
+        }
+      }
+    }
+  };
 
   // 组件挂载完毕之前触发 初始化一些参数
   componentWillMount() {
-    if(this.props.options) {
+    if (this.props.options) {
       this.setState({
         options: Object.assign({}, this.state.options, this.props.options),
       });
     }
-    if(this.props.iscrollOptions) {
+    if (this.props.iscrollOptions) {
       this.setState({
         iscrollOptions: Object.assign({}, this.state.iscrollOptions, this.props.iscrollOptions),
       });
@@ -87,47 +88,47 @@ class IscrollLuo extends React.Component {
 
   // 组件初始化完毕时触发
   componentDidMount() {
-      this.myScroll = new IScroll(`#${this.props.id}_warpper`, Object.assign({}, this.state.iscrollOptions, this.props.iscrollOptions));
+    this.myScroll = new IScroll(`#${this.props.id}_warpper`, Object.assign({}, this.state.iscrollOptions, this.props.iscrollOptions));
 
-      this.myScroll.on('scroll', () => {
-        const myScroll = this.myScroll;
+    this.myScroll.on('scroll', () => {
+      const myScroll = this.myScroll;
 
-        if(myScroll.y >= this.state.options.beyondHeight && !this.state.yesDown) {
-          this.setState({
-            yesDown: true,
-          });
-        } else if (myScroll.y > 0 && myScroll.y < this.state.options.beyondHeight && this.state.yesDown) {
-          this.setState({
-            yesDown: false,
-          });
-        } else if (myScroll.y < myScroll.maxScrollY && myScroll.y > myScroll.maxScrollY - this.state.options.beyondHeight && this.state.yesUp) {
-          this.setState({
-            yesUp: false,
-          });
-        } else if (myScroll.y < myScroll.maxScrollY - this.state.options.beyondHeight && !this.state.yesUp) {
-          this.setState({
-            yesUp: true,
-          });
-        }
-      });
-
-      this.myScroll.on('scrollStart', () => {
-        window.top.addEventListener('mouseup', this.onMouseUpListener, false);
-        window.top.addEventListener('touchend', this.onMouseUpListener, false);
-      });
-
-      this.setState({
-        data: this.props.children,
-      });
-
-      if(this.props.detectionHeight) {
-        this.onDetectionHeight();
+      if (myScroll.y >= this.state.options.beyondHeight && !this.state.yesDown) {
+        this.setState({
+          yesDown: true,
+        });
+      } else if (myScroll.y > 0 && myScroll.y < this.state.options.beyondHeight && this.state.yesDown) {
+        this.setState({
+          yesDown: false,
+        });
+      } else if (myScroll.y < myScroll.maxScrollY && myScroll.y > myScroll.maxScrollY - this.state.options.beyondHeight && this.state.yesUp) {
+        this.setState({
+          yesUp: false,
+        });
+      } else if (myScroll.y < myScroll.maxScrollY - this.state.options.beyondHeight && !this.state.yesUp) {
+        this.setState({
+          yesUp: true,
+        });
       }
+    });
+
+    this.myScroll.on('scrollStart', () => {
+      window.top.addEventListener('mouseup', this.onMouseUpListener, false);
+      window.top.addEventListener('touchend', this.onMouseUpListener, false);
+    });
+
+    this.setState({
+      data: this.props.children,
+    });
+
+    if (this.props.detectionHeight) {
+      this.onDetectionHeight();
+    }
   }
 
   /** children内容改变时触发,表示已完成了刷新或加载 **/
-    componentWillReceiveProps(nextProps) {
-    if(this.props.children !== nextProps.children) {
+  componentWillReceiveProps(nextProps) {
+    if (this.props.children !== nextProps.children) {
       this.setState({
         data: nextProps.children,
         loadingDownShow: false,
@@ -137,22 +138,22 @@ class IscrollLuo extends React.Component {
   }
 
   /** children内容改变时触发,表示已完成了刷新或加载 **/
-    static getDerivedStateFromProps(nextP, prevState) {
-        if (nextP.children !== prevState.data) {
-            return {
-                data: nextP.children,
-                loadingDownShow: false,
-                loadingUpShow: false,
-            };
-        }
-        return null;
+  static getDerivedStateFromProps(nextP, prevState) {
+    if (nextP.children !== prevState.data) {
+      return {
+        data: nextP.children,
+        loadingDownShow: false,
+        loadingUpShow: false,
+      };
     }
+    return null;
+  }
 
-    componentDidUpdate(prevP, prevS){
-      if(prevS.data !== this.state.data || prevP.noDown !== this.props.noDown || prevP.noUp !== this.props.noUp) {
-        this.onRefresh();
-      }
+  componentDidUpdate(prevP, prevS) {
+    if (prevS.data !== this.state.data || prevP.noDown !== this.props.noDown || prevP.noUp !== this.props.noUp) {
+      this.onRefresh();
     }
+  }
 
   /** 组件即将销毁时触发，销毁当前iscroll实例 **/
   componentWillUnmount() {
@@ -164,14 +165,14 @@ class IscrollLuo extends React.Component {
   onDetectionHeight() {
     const that = this;
     clearTimeout(that.iscrollTimer);
-    that.iscrollTimer = setTimeout(function(){
+    that.iscrollTimer = setTimeout(function() {
       that.iscrollTimeout(that);
-   }, 500);
+    }, 500);
   }
 
   iscrollTimeout(me) {
     const dom = me.docStatus;
-    if(dom && dom.offsetHeight !== me.state.boxHeight) {
+    if (dom && dom.offsetHeight !== me.state.boxHeight) {
       me.setState({
         boxHeight: dom.offsetHeight,
       });
@@ -180,12 +181,12 @@ class IscrollLuo extends React.Component {
     me.iscrollHeightChange();
   }
 
- iscrollHeightChange() {
+  iscrollHeightChange() {
     const me = this;
     clearTimeout(me.iscrollTimer);
-    me.iscrollTimer = setTimeout(function(){
+    me.iscrollTimer = setTimeout(function() {
       me.iscrollTimeout(me);
-   }, 700);
+    }, 700);
   }
 
   /** 刷新ISCROLL **/
@@ -201,37 +202,39 @@ class IscrollLuo extends React.Component {
     return (
       <div
         id={this.props.id}
-        ref={(dom) => this.docStatus = dom}
+        ref={dom => (this.docStatus = dom)}
         className={this.props.className ? `iscroll-luo-box ${this.props.className}` : 'iscroll-luo-box'}
-        style={{ backgroundColor: this.state.options.backgroundColor }}
-      >
-        <div className={this.state.loadingDownShow && !this.props.noDown ? 'sl_down sl_show' : 'sl_down'} style={{ backgroundColor: this.state.options.backgroundColor, color: this.state.options.fontColor }}>
+        style={{ backgroundColor: this.state.options.backgroundColor }}>
+        <div
+          className={this.state.loadingDownShow && !this.props.noDown ? 'sl_down sl_show' : 'sl_down'}
+          style={{ backgroundColor: this.state.options.backgroundColor, color: this.state.options.fontColor }}>
           <img src={iconLoading} />
           {this.state.options.pulldowningInfo}
         </div>
-        <div
-          id={`${this.props.id}_warpper`}
-          className="sl_scroller"
-        >
+        <div id={`${this.props.id}_warpper`} className="sl_scroller">
           <div>
             <div className="scroller-pullDown">
               <span className={this.state.yesDown ? 'icon reverse_icon' : 'icon'} style={{ display: this.props.noDown ? 'none' : 'inline' }}>
                 <img src={iconSldown} />
               </span>
-              <span className = "msg" style={{ color: this.state.options.fontColor, display: this.props.noDown ? 'none' : 'inline' }}>
+              <span className="msg" style={{ color: this.state.options.fontColor, display: this.props.noDown ? 'none' : 'inline' }}>
                 {this.state.yesDown ? this.state.options.pulldownReadyInfo : this.state.options.pulldownInfo}
               </span>
-                <span className="msg" style={{ display: this.props.noDown ? 'inline' : 'none' }}>{this.props.noDownStr}</span>
+              <span className="msg" style={{ display: this.props.noDown ? 'inline' : 'none' }}>
+                {this.props.noDownStr}
+              </span>
             </div>
             <div className="scroller-content">{this.props.children}</div>
             <div className="scroller-pullUp">
               <span className={this.state.yesUp ? 'icon reverse_icon' : 'icon'} style={{ display: this.props.noUp ? 'none' : 'inline' }}>
                 <img src={iconSlup} />
               </span>
-              <span className = "msg" style={{ color: this.state.options.fontColor, display: this.props.noUp ? 'none' : 'inline' }}>
+              <span className="msg" style={{ color: this.state.options.fontColor, display: this.props.noUp ? 'none' : 'inline' }}>
                 {this.state.yesUp ? this.state.options.pullupReadyInfo : this.state.options.pullupInfo}
               </span>
-                <span className = "msg" style={{ display: this.props.noUp ? 'inline' : 'none' }}>{this.props.noUpStr}</span>
+              <span className="msg" style={{ display: this.props.noUp ? 'inline' : 'none' }}>
+                {this.props.noUpStr}
+              </span>
             </div>
           </div>
         </div>
